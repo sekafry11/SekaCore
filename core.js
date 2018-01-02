ec0db91db710c1fa3471dc3416b4162fb84e0cf1
sekafry11 <32816909+sekafry11@users.noreply.github.com>
2018-01-02 23:05:02 +0100

    Create core.js

---
+++ core.js
@@ -0,0 +1,13 @@
+function newNode(name, p, o){
+  var a = document.createElement(name), b, c;
+  a.setAttr = function(a, b){
+    this.setAttribute(a, b);
+    return this;
+  };
+  o = o || {};
+  for(b in o){
+    a[b] = o[b];
+  }
+  if(p){p.appendChild(a);}
+  return a;
+}
