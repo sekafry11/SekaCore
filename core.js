cd0fe85b0022b0d1b5235454d4773ceff3bcc082
sekafry11 <a0torres0a@gmail.com>
2018-01-02 22:58:35 +0100

    Core V1.0

--- core.js
+++ core.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function newNode(name, p, o){
   var a = document.createElement(name), b, c;
   a.setAttr = function(a, b){
@@ -12,9 +11,3 @@ function newNode(name, p, o){
   if(p){p.appendChild(a);}
   return a;
 }
-<<<<<<< HEAD
-=======
-=======
-º
->>>>>>> ecb0087c5097ca4c0e683d48d2fe0f4d4f21e692
->>>>>>> d04603f71e385eb368f697ef6db7d78e8dcb8dfb
