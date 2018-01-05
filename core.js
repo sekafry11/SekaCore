eaaa84a358c8299b7975f65425caf7af0fb7503e
sekafry11 <a0torres0a@gmail.com>
2018-01-05 22:38:40 +0100

    function ajax

--- core.js
+++ core.js
@@ -11,3 +11,13 @@ function newNode(name, p, o){
   if(p){p.appendChild(a);}
   return a;
 }
+function ajax(n, f){
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        f(xhttp.responseText);
+      }
+  };
+  xhttp.open("GET", n, true);
+  xhttp.send();
+}
