d9e6536530c193df5bee681c6b137274db4073f0
sekafry11 <a0torres0a@gmail.com>
2018-01-24 11:57:56 +0100

    Add newTable function.

--- core.js
+++ core.js
@@ -1,4 +1,4 @@
-function newNode(name, p, o){
+function newNode(name, p, o){ // Node Name, parent, atributos
   var a = document.createElement(name), b, c;
   a.setAttr = function(a, b){
     this.setAttribute(a, b);
@@ -11,10 +11,10 @@ function newNode(name, p, o){
   if(p){p.appendChild(a);}
   return a;
 }
-function ajax(n, f){
+function ajax(n, f, c){ // url, function
   f = f || function(){};
   var xhttp = new XMLHttpRequest();
-  xhttp.withCredentials = true;
+  xhttp.withCredentials = (c == false)?false:true;
   xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
         f(xhttp.responseText);
@@ -22,4 +22,15 @@ function ajax(n, f){
   };
   xhttp.open("GET", n, true);
   xhttp.send();
+}
+function newTable(t, a, b, f, o){ // table, ancho, alto, function, object
+  var aa, bb, tr;
+  t = t || newNode("table");
+  for(aa = 0; aa < a; ++aa){
+    tr = newNode("tr", t)
+    for(bb = 0; bb < b; ++bb){
+      f(tr, aa, bb, o); //tr, x, y, object;
+    }
+  }
+  return t;
 }
